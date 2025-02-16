1e4a273b1ed7179d2397e5c9b476d334a5e88b53
jackfromeast <thisiszhengyu@gmail.com>
2025-02-16 12:55:35 -0500

    [Downloader] Update the downloader to support downloading github/pip based on its url types.

--- analyzer/src/workflows/run_cp_analysis.py
+++ analyzer/src/workflows/run_cp_analysis.py
@@ -58,7 +58,7 @@ class ClassPollutionAnalysis(BaseScheduler):
     """
     repo_workspace_path = self.setup_workspace_for_repo(repo_url)
 
-    if not download(repo_url, repo_workspace_path, pip=self.config.SCHEDULER.SOURCE=="PIP"):
+    if not download(repo_url, repo_workspace_path):
       self.logger.error(f"Failed to download codebase for {repo_url}")
       self.cleanup_folders(repo_workspace_path)
       return
