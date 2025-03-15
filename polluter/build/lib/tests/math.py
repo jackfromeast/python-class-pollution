e0f0eea42a8c393ec0f86f67d244b19eba166cce
jackfromeast <thisiszhengyu@gmail.com>
2025-03-14 23:05:45 -0400

    [Polluter] Update the polluter to support select object, class, dict, and so on.

--- polluter/build/lib/tests/math.py
+++ polluter/build/lib/tests/math.py
@@ -1,14 +1,11 @@
 import math
-from polluter import Pollutable
-import polluter as pl
+from src.pollutable import Pollutable
 
-po = Pollutable(pl, lookup_type="getAttr")
+po = Pollutable(math, lookup_type="getAttr")
 modules = po.select("type=module")
-callables = po.select("type=callable")
+callables = po.select("type=noncallable")
 dicts = po.select("type=dict")
 classes = po.select("type=class") 
-strings = po.select("type=string")
-objects = po.select("type=object")
 
 print("Modules:")
 for path, value in modules.items():
@@ -25,12 +22,4 @@ for path, value in dicts.items():
 
 print("\nClasses:")
 for path, value in classes.items():
-  print(f"{path}: {value}")
-
-print("\nStrings:")
-for path, value in strings.items():
-  print(f"{path}: {value}")
-
-print("\nObjects:")
-for path, value in objects.items():
   print(f"{path}: {value}")
--- polluter/setup.py
+++ polluter/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="polluter",
-    version="0.1",
-    packages=find_packages(),
+    version="0.3.3",
+    author="j@ack",
+    packages=find_packages(),  # Automatically find packages in the root directory
 )
