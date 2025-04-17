43721e600ce69903bdf977525e34457946b33236
jackfromeast <thisiszhengyu@gmail.com>
2025-04-17 11:23:51 -0400

    [Polluter] Update the polluter packages.

--- polluter/setup.py
+++ polluter/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="polluter",
-    version="0.3.3",
+    version="0.3.4",
     author="j@ack",
     packages=find_packages(),  # Automatically find packages in the root directory
 )
