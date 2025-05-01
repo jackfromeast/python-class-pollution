97537d27e2c0a5cb63c57e01c891323f63232e64
jackfromeast <thisiszhengyu@gmail.com>
2025-05-01 11:53:26 -0400

    [Scripts] Fix a bug when extracting the function name.

--- scripts/summary_class_pollution_func.py
+++ scripts/summary_class_pollution_func.py
@@ -30,7 +30,7 @@ for line in open(repo_list_path):
       physical = loc.get("physicalLocation", {})
       uri = physical.get("artifactLocation", {}).get("uri")
       region = physical.get("region", {})
-      func_name = loc.get("message", {}).get("text", "").strip("Function ")
+      func_name = loc.get("message", {}).get("text", "").lstrip("Function ")
 
       if func_name in func_names:
         continue
