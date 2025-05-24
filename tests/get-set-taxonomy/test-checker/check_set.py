29846655159d5513fb4076150ba3874991c99755
MuxiLyuLucy <muxilyu2004@gmail.com>
2025-05-24 05:28:09 +0000

    [Tests] Update tests to match setters in the table

--- tests/get-set-taxonomy/test-checker/check_set.py
+++ tests/get-set-taxonomy/test-checker/check_set.py
@@ -20,37 +20,47 @@ class TestSetOperations(unittest.TestCase):
         setattr_dunder(self.obj, "bar", 123)
         self.assertEqual(self.obj.bar, 123)
 
+    # #3 object.__setattr__(obj, name, val)
+    def test_object_setattr_dunder(self):
+        object_setattr_dunder(self.obj, "bar", 123)
+        self.assertEqual(self.obj.bar, 123)
+
     # special case: type(obj).__setattr__(obj, name, val)
     def test_type_setattr(self):
         type_setattr(self.obj, "baz", "hello")
         self.assertEqual(self.obj.baz, "hello")
 
-    # #3 obj.__dict__[name] = val
+    # #4 obj.__dict__[name] = val
     def test_dict_dunder_set(self):
         dict_dunder_set(self.obj, "x", "value")
         self.assertEqual(self.obj.__dict__["x"], "value")
 
-    # #4 dict[key] = val
+    # #5 dict[key] = val
     def test_item_set(self):
         item_set(self.d, "a", 10)
         self.assertEqual(self.d["a"], 10)
 
-    # #5 dict.update(key=val)
+    # #6 dict.setdefault(key, val)
+    def test_dict_update(self):
+        dict_setdefault(self.d, "b", 20)
+        self.assertEqual(self.d["b"], 20)
+
+    # #7 dict.update(key=val)
     def test_dict_update(self):
         dict_update(self.d, "b", 20)
         self.assertEqual(self.d["b"], 20)
 
-    # #6 dict.__setitem__(key, val)
+    # #8 dict.__setitem__(key, val)
     def test_setitem_dunder(self):
         setitem_dunder(self.d, "c", 30)
         self.assertEqual(self.d["c"], 30)
 
-    # #7 operator.setitem(dict, key, val)
+    # #9 operator.setitem(dict, key, val)
     def test_setitem_operator(self):
         setitem_operator(self.d, "d", 40)
         self.assertEqual(self.d["d"], 40)
 
-    # #8 operator.__setitem__(dict, key, val)
+    # #10 operator.__setitem__(dict, key, val)
     def test_setitem_dunder_operator(self):
         setitem_dunder_operator(self.d, "e", 50)
         self.assertEqual(self.d["e"], 50)
--- tests/get-set-taxonomy/test-codebase/setter.py
+++ tests/get-set-taxonomy/test-codebase/setter.py
@@ -2,36 +2,44 @@
 def setattr_builtin(obj, name, val):
     setattr(obj, name, val)
 
-#2 object.__setattr__(obj,name,val)
+#2 obj.__setattribute__(name,val)
 def setattr_dunder(obj, name, val):
+    obj.__setattr__(name,val)
+
+#3 object.__setattr__(obj,name,val)
+def object_setattr_dunder(obj, name, val):
     object.__setattr__(obj, name, val)
 
 # special case of object.__setattr__(obj,name,val)
 def type_setattr(obj, name, val):
     type(obj).__setattr__(obj, name, val)
 
-#3 obj.__dict__[name]=val 
+#4 obj.__dict__[name]=val 
 def dict_dunder_set(obj, name, val):
     obj.__dict__[name] = val
 
-#4 dict[key]=val
+#5 dict[key]=val
 def item_set(d, k, v):
     d[k] = v
 
-#5 dict.update(key=val)
+#6 dict.setdefault(key, val)
+def dict_setdefault(d, k, v):
+    d.setdefault(k, v)
+
+#7 dict.update(key=val)
 def dict_update(d, k, v):
     d.update({k: v})
 
-#6 dict.__setitem__(key, val) 
+#8 dict.__setitem__(key, val) 
 def setitem_dunder(obj, key, val):
     obj.__setitem__(key, val)
 
-#7 operator.setitem(dict,key,val)
+#9 operator.setitem(dict,key,val)
 import operator
 def setitem_operator(d, k, v):
     operator.setitem(d, k, v)
 
-#8 operator.__setitem__(dict,key,val)
+#10 operator.__setitem__(dict,key,val)
 import operator
 def setitem_dunder_operator(d, k, v):
     operator.__setitem__(d, k, v)
