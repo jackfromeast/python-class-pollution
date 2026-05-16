7284f6134281e72eb438a8c6b85c296b0dcc907f
jackfromeast <thisiszhengyu@gmail.com>
2026-05-15 23:45:41 -0400

    build: remove manual pip dependency installation
    Remove the pip installation commands from the setup script. This streamlines the installation process and delegates Python dependency management to the environment or a separate configuration.

--- install.sh
+++ install.sh
@@ -16,7 +16,3 @@ codeql pack install
 # Compile query
 print_command "codeql query compile class-pollution.qls"
 codeql query compile class-pollution.qls
-
-# Install pip dependency
-print_command "python3 -m pip install psutil PyYAML packageurl-python ruamel.yaml"
-python3 -m pip install psutil PyYAML packageurl-python ruamel.yaml langgraph langchain-deepseek
